9b00c2cf81be9589d30db76619f0a45b95967bc4
lukewarkentin <luke.warkentin@gmail.com>
2026-02-24 09:51:23 -0800

    Function description, license

--- R/cpue.R
+++ R/cpue.R
@@ -1,3 +1,17 @@
+#' Calculate catch per unit effort (CPUE)
+#'
+#'
+#'
+#' @param catch Numeric vector of catch
+#' @param effort Numeric vector of effort
+#' @param gear_factor Numeric vector for gear standardize
+#'
+#' @returns Numeric vector of CPUE values
+#' @export
+#'
+#' @examples
+#' cpue(100,10)
+#' cpue(100,10, gear_factor = 1.5)
 cpue <- function(catch, effort, gear_factor = 1) {
   raw_cpue <- catch / effort
 
